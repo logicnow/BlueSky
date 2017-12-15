5b653d354c7b4d2958840691bdccecd207597674
sphen13 <sboyle@trmacs.com>
2017-12-15 12:14:25 -0500

    delay fail2ban-client reload

--- docker/fail2ban-supervisor.sh
+++ docker/fail2ban-supervisor.sh
@@ -9,5 +9,5 @@ trap 'on_die' TERM
 # This simulates the normal startup, only in the foreground
 # Not using fail2ban-client to ensure supervisor catches any exits
 /usr/bin/python3 /usr/bin/fail2ban-server -s /var/run/fail2ban/fail2ban.sock -p /var/run/fail2ban/fail2ban.pid -f &
-sleep 1;fail2ban-client reload
+sleep 5;/usr/bin/fail2ban-client reload
 wait
